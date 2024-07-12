025156ecdeac7f41a6b03eeb5e75a178933efb17
VictorNAGomes <dev.victorgomes@gmail.com>
2024-07-12 19:53:42 -0300

    feat: create insert seller functionality

--- app/Http/Controllers/SellerController.php
+++ app/Http/Controllers/SellerController.php
@@ -7,6 +7,43 @@ use Illuminate\Http\Request;
 
 class SellerController extends Controller
 {
+    public function create(Request $request)
+    {
+        if (!$request->name or $request->name == null or $request->name == '') {
+            return response()->json([
+                'status' => false,
+                'message' => 'Nome Inválido'
+            ], 400);
+        }
+
+        if (!$request->email or $request->email == null or $request->email == '') {
+            return response()->json([
+                'status' => false,
+                'message' => 'Email Inválido'
+            ], 400);
+        }
+
+        $sellerAlreadyExists = Seller::where('email', $request->email)->get();
+        if (count($sellerAlreadyExists->toArray()) > 0) {
+            return response()->json([
+                'status' => false,
+                'message' => 'Email já cadastrado'
+            ], 400);
+        }
+
+        $seller = new Seller;
+
+        $seller->name = $request->name;
+        $seller->email = $request->email;
+
+        $seller->save();
+
+        return response()->json([
+            'status' => true,
+            'message' => 'Vendedor Criado com sucesso'
+        ], 200);
+    }
+
     public function getAll()
     {
         $sellers = Seller::all();
@@ -18,6 +55,12 @@ class SellerController extends Controller
     {
         $seller = Seller::where('id', $id)->get();
 
+        if (count($seller->toArray()) == 0) {
+            return response()->json([
+                'status' => false,
+                'message' => 'Vendedor não encontrado'
+            ], 404);
+        }
         return $seller->toArray();
     }
 }
